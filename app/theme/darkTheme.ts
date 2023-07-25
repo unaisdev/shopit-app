aae8e6a8af83f8049140489181b23562c4c15ebe
alfonso <alfonsolasaguirre@gmail.com>
2023-07-26 00:35:02 +0200

    add modal button

--- app/theme/darkTheme.ts
+++ app/theme/darkTheme.ts
@@ -2,6 +2,7 @@ const DarkTheme = {
   colors: {
     dark: '#F2F2F2',
     white: '#151515',
+    gray: '#A4A4A4',
   },
 } as const;
 
--- app/theme/lightTheme.ts
+++ app/theme/lightTheme.ts
@@ -2,6 +2,7 @@ const LightTheme = {
   colors: {
     dark: '#151515',
     white: '#F7F7F7',
+    gray: '#CBCBCB',
   },
 } as const;
 
